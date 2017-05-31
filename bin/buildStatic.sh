04546b508adce301fecd686831837b87b4f150a8
Alex Smith <altaudiolabs@gmail.com>
2017-05-31 15:07:25 +0100

    Only copies fonts and scss folders from ionicons

--- bin/buildStatic.sh
+++ bin/buildStatic.sh
@@ -1,5 +1,6 @@
 rm -rf ./static && \
   mkdir ./static && \
-  cp -a ./images/. ./static/ && \
-  cp -a ./node_modules/bootstrap-sass/. ./static/bootstrap-sass/ && \
-  cp -a ./node_modules/ionicons-pre/. ./static/ionicons-pre/
+  cp -r ./images/. ./static/ && \
+  cp -r ./node_modules/bootstrap-sass/. ./static/bootstrap-sass/ && \
+  cp -r ./node_modules/ionicons-pre/scss/. ./static/ionicons-scss && \
+  cp -r ./node_modules/ionicons-pre/fonts/. ./static/ionicons-fonts
