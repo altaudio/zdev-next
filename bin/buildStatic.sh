c3d6d0bea1585478dd30484faf73ec7e8928952c
Alex Smith <altaudiolabs@gmail.com>
2017-05-31 14:36:34 +0100

    Adds bash script to build static directory

---
+++ bin/buildStatic.sh
@@ -0,0 +1,5 @@
+rm -rf ./static && \
+  mkdir ./static && \
+  cp -a ./images/. ./static/ && \
+  cp -a ./node_modules/bootstrap-sass/. ./static/bootstrap-sass/ && \
+  cp -a ./node_modules/ionicons-pre/. ./static/ionicons-pre/
